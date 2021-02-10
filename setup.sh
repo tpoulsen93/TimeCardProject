43f801aa29fa0bbc359a544f89cf32cbea46f601
tpoulsen <t-poulsen@hotmail.com>
2021-02-09 17:42:20 -0700

    updated the README and setup

--- setup.sh
+++ setup.sh
@@ -8,16 +8,18 @@ echo
 javac *.java
 echo
 
-echo "install xlsx2csv to convert excel spreadsheet to csv..."
+echo "install xlsx2csv to convert excel spreadsheet to csv...(Ubuntu)"
 echo
 sudo apt-get install xlsx2csv
 echo
 
-echo "install sendmail to send texts and emails"
+echo "install ssmtp to send texts and emails"
 echo
 sudo apt-get install ssmtp
 echo
 
 echo
 echo "Look up how to configure ssmtp and do it."
+echo "Create the employee database using this format:"
+echo "firstname [sheetId]"
 echo "Then you should be ready to rumble.... I hope..."
