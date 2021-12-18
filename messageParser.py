509b0153ec839d904c3240ac19893b09605b504c
NotCarpeDM <47906837+NotCarpeDM@users.noreply.github.com>
2021-12-18 14:56:37 -0700

    initial twilio progress

---
+++ messageParser.py
@@ -0,0 +1,14 @@
+
+
+
+def _is_valid_message(message: str):
+    # break the message apart into an array
+    arr = message.split()
+
+    # check each element of the array to make sure it is valid
+    if arr[0]
+
+
+
+def parseMessage(message: str):
+    
--- twilioTest.py
+++ twilioTest.py
@@ -1,21 +1,54 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
+import json
 import os
 from twilio.rest import Client
+from dotenv import load_dotenv
+from datetime import date
+
+from fastapi import FastAPI, Request, Form
+from pydantic import BaseModel
+
+load_dotenv()
+account = os.environ.get('account_sid')
+token = os.environ.get('auth_token')
+
+app = FastAPI()
 
 
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
-# account_sid = os.environ['AC37804c695edc1fb632643fbfff0c3245']
-# auth_token = os.environ['2a47bca83b62c8db87c9f801b6b7a4e8']
-account_sid = 'AC37804c695edc1fb632643fbfff0c3245'
-auth_token = '2a47bca83b62c8db87c9f801b6b7a4e8'
-client = Client(account_sid, auth_token)
 
-message = client.messages \
-                .create(
-                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_='+18324971734',
-                     to='+12083500006'
-                 )
+# client = Client(account_sid, auth_token)
 
-print(message.sid)
+# message = client.messages \
+#                 .create(
+#                      body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+#                      from_='+18324971734',
+#                      to='+12083500006'
+#                  )
+
+# print(message.sid)
+
+users = {
+    "+12083500006": "Taylor Poulsen",
+    "+14322761331": "Daniel Bogden"
+}
+
+
+class SMS(BaseModel):
+    From: str
+    Body: str
+
+
+@app.get("/")
+def read_root():
+    today = date.today()
+    return {"msg": f"Hello, Taylor.  Today is {today}"}
+
+
+@app.post("/sms")
+async def response(From: str = Form(...), Body: str = Form(...)) -> str:
+    print(f"Mesage from: {From}")
+    print(f"Body: {Body}")
+    pass
+    
+
+
